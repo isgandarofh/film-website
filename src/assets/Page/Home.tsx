1ce71f91a1e5464e22195dd5c1b27302a382eb26
Your Name <you@example.com>
2026-06-04 12:17:29 +0400

    questions section finish

--- src/assets/Page/Home.tsx
+++ src/assets/Page/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from '../components/Header/Header'
 import FirstSection from '../components/FirstSection/FirstSection'
 import CategoriesSection from '../components/CategoriesSection/CategoriesSection'
